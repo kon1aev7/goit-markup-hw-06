cf627721c80d26ad5d255deff8b3234030956692
kon1aev7 <konyaevd1995@gmail.com>
2022-01-04 02:33:17 +0200

    форматировка

--- js/main.js
+++ js/main.js
@@ -1,19 +1,22 @@
 (() => {
   const refs = {
-    formsend: document.querySelector('.js-speaker-form'),
-    modalsend:document.querySelector('.modal-container')};
+    formsend: document.querySelector(".js-speaker-form"),
+    modalsend: document.querySelector(".modal-container"),
+  };
 
-  refs.formsend.addEventListener('submit', e => {
+  refs.formsend.addEventListener("submit", (e) => {
     e.preventDefault();
     new FormData(e.currentTarget).forEach((value, name) =>
-      console.log(`${name}: ${value}`));
+      console.log(`${name}: ${value}`)
+    );
     e.currentTarget.reset();
   });
 
-   refs.modalsend.addEventListener('submit', e => {
+  refs.modalsend.addEventListener("submit", (e) => {
     e.preventDefault();
     new FormData(e.currentTarget).forEach((value, name) =>
-      console.log(`${name}: ${value}`));
+      console.log(`${name}: ${value}`)
+    );
     e.currentTarget.reset();
   });
 })();
--- js/modal.js
+++ js/modal.js
@@ -1,18 +1,16 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-    formmodal: document.querySelector('[data-form-modal]')
-
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+    formmodal: document.querySelector("[data-form-modal]"),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-    refs.formmodal.classList.toggle('is-hidden');
+    refs.modal.classList.toggle("is-hidden");
+    refs.formmodal.classList.toggle("is-hidden");
   }
 })();
